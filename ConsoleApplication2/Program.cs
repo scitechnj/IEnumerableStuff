7a39c3a34c236d940974151d0049066a5d5a921f
Avrumi Friedman <datmonkey2002@yahoo.com>
2013-11-04 22:40:44 -0500

    For shaya

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,9 +11,35 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            MyRandomGenerator rnd = new MyRandomGenerator(10, 1000, 10);
-            IEnumerable<int> randomEvenNumbers = EnumerableHelpers.FindAll(rnd, MyPredicateMethod);
-            foreach (int x in randomEvenNumbers)
+            //MyRandomGenerator rnd = new MyRandomGenerator(10, 1000, 10);
+            //IEnumerable<int> randomEvenNumbers = EnumerableHelpers.FindAll(rnd, MyPredicateMethod);
+            //foreach (int x in randomEvenNumbers)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person
+                {
+                    FirstName = "Alex",
+                    LastName = "Friedman",
+                    Age = 31
+                });
+            persons.Add(new Person
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 45
+            });
+            persons.Add(new Person
+            {
+                FirstName = "Mike",
+                LastName = "Jones",
+                Age = 65
+            });
+
+            IEnumerable<int> ages = EnumerableHelpers.Map<Person, int>(persons, MapMethod);
+            foreach (int x in ages)
             {
                 Console.WriteLine(x);
             }
@@ -25,6 +51,13 @@ namespace ConsoleApplication2
         {
             return x % 2 == 0;
         }
+
+        private static int MapMethod(Person p)
+        {
+            return p.Age;
+        }
+
+
     }
 
     public static class EnumerableHelpers
@@ -43,6 +76,38 @@ namespace ConsoleApplication2
 
             return list;
         }
+
+        public static T First<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            foreach (T item in items)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
+            return default(T);
+        }
+
+        public static IEnumerable<T1> Map<T, T1>(IEnumerable<T> items, Func<T, T1> func)
+        {
+            List<T1> result = new List<T1>();
+            foreach (T item in items)
+            {
+                result.Add(func(item));
+            }
+
+            return result;
+        }
+
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 
     public class MyRandomGenerator : IEnumerable<int>
